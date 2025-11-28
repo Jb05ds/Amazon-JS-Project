b44232d89bff7f107689322f37cff9d08cff9ac2
John Bert Delos Santos <johnbertdelossantos480@gmail.com>
2025-11-28 20:45:37 +0800

    modular price formatting and fixed radio selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,8 @@ import {products} from '../data/products.js'
 
 import {cart, addToCart} from '../data/cart.js';
 
+import { formatCurrency } from './utils/money.js';
+
 let productHTML = '';
 
 products.forEach((product) => {
@@ -25,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
