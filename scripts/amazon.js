008fb00e44c0b5a2499278cb975cfc703b22916a
jbds <johnbertdelossantos480@gmail.com>
2026-03-04 16:49:12 +0800

    some tests on add to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -31,7 +31,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select class="js-quantity-selector-${product.id}">
+            <select>
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
